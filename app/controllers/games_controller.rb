34469c99aaa63db7ced9a5cdabef7ea4fc4944b2
Jonathan Chang <jonoschang7@gmail.com>
2015-09-16 21:55:15 -0400

    implement new and create action for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,4 +6,21 @@ class GamesController < ApplicationController
 	def show
 		@game = Game.find(params[:id])
 	end
+
+	def new
+		@game = Game.new
+	end
+
+	def create
+		@game = Game.new(game_params)
+		if @game.save
+			redirect_to @game
+		end
+	end
+end
+
+private
+
+def game_params
+	params.require(:game).permit(:name, :players, :amount_per_card)
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -30,4 +30,29 @@ RSpec.describe GamesController, type: :controller do
   		expect(response).to render_template :show
   	end
   end
+
+  describe 'GET #new' do
+  	it "assigns a new game to @game" do
+  		get :new
+  		expect(assigns(:game)).to be_a_new(Game)
+  	end
+  end
+
+  describe 'POST #create' do
+  	context "with valid attributes" do
+  		it "saves the new game in the database" do
+  			expect{
+  				post :create, game: attributes_for(:game)
+  			}.to change(Game, :count).by(1)
+  		end
+
+  		it "redirects to game #show" do
+  			post :create, game: attributes_for(:game)
+  			expect(response).to redirect_to game_path(assigns(:game))
+  	  end
+  	end
+
+  	context "with invalid attributes" do
+  	end
+  end
 end
