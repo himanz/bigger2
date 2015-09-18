b056f24369776522cc72d60932701d0821b18d81
Jonathan Chang <jonoschang7@gmail.com>
2015-09-18 11:57:22 -0400

    testing new and create actions for rosters

--- spec/controllers/rosters_controller_spec.rb
+++ spec/controllers/rosters_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe RostersController, type: :controller do
+  describe 'GET #new' do
+  	it "assigns a new roster to @roster" do
+  		get :new
+  		expect(assigns(:roster)).to be_a_new(Roster)
+  	end
+  end
 
+  describe 'POST #create' do
+  	context "with valid attributes" do
+  		it "saves the new roster in the database" do
+  			expect{
+  				post :create, roster: attributes_for(:roster)
+  			}.to change(Roster, :count).by(1)
+  		end
+  	end
+
+  	context "with invalid attributes" do
+  		it "does not save the new roster in the database" do
+	  		expect{
+	  			post :create, roster: attributes_for(:roster, game_id: nil)
+	  		}.to change(Roster, :count).by(0)
+  	  end
+  	end
+  end
 end
