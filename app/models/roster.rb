fb4febe7712995790bf928b14397c8d153c12f92
Jonathan Chang <jonoschang7@gmail.com>
2015-09-18 11:53:49 -0400

    tests for validity of roster model

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -1,4 +1,6 @@
 class Roster < ActiveRecord::Base
 	belongs_to :game
 	belongs_to :player
+
+	validates :game_id, :player_id, presence: true
 end
--- spec/models/roster_spec.rb
+++ spec/models/roster_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Roster, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a game_id and player_id" do
+  	roster = create(:roster)
+  	expect(roster).to be_valid
+  end
+
+  it "is invalid without a game_id" do
+  	roster = build(:roster, game_id: nil)
+  	roster.valid?
+  	expect(roster.errors[:game_id].size).to eq(1)
+  end
+
+  it "is invalid without a player_id" do
+  	roster = build(:roster, player_id: nil)
+  	roster.valid?
+  	expect(roster.errors[:player_id].size).to eq(1)
+  end
 end
